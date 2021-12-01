e598523f6d432011d3655821077abd3d84f54149
nishant <nishant@devtron.ai>
2021-12-01 14:45:21 +0530

    local token tested

--- go.mod
+++ go.mod
@@ -3,13 +3,31 @@ module github.com/devtron-labs/authenticator
 go 1.16
 
 require (
-	github.com/caarlos0/env/v6 v6.7.2 // indirect
+	github.com/caarlos0/env/v6 v6.7.2
 	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.1.0
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/googleapis/gnostic v0.0.0-20170729233727-0c5108395e2d // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/json-iterator/go v0.0.0-20180612202835-f2b4162afba3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180320133207-05fbef0ca5da // indirect
 	github.com/pquerna/cachecontrol v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v0.0.0-20170130214245-9ff6c6923cff // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	google.golang.org/grpc v1.31.0
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/api v0.0.0-20191004102349-159aefb8556b
+	k8s.io/apimachinery v0.0.0-20190816221834-a9f1d8a9c101
+	k8s.io/client-go v11.0.1-0.20190820062731-7e43eff7c80a+incompatible
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20191010214722-8d271d903fe4 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/devtron-labs/authenticator/middleware"
@@ -22,10 +23,11 @@ func main() {
 	flag.Parse()
 
 	dexConfig := &oidc.DexConfig{
-		DexServerAddress: *dexServerAddress,
-		Url:              *url,
-		DexClientSecret:  *dexClientSecret,
-		DexClientID:      *dexCLIClientID,
+		DexServerAddress:           *dexServerAddress,
+		Url:                        *url,
+		DexClientSecret:            *dexClientSecret,
+		DexClientID:                *dexCLIClientID,
+		UserSessionDurationSeconds: 10000,
 	}
 	userVerier := func(email string) bool { return true }
 	redirectUrlSanitiser := func(url string) string { return url }
@@ -40,6 +42,7 @@ func main() {
 		return
 	}
 	sessionManager := middleware.NewSessionManager(settings, dexConfig.DexServerAddress)
+	loginService := middleware.NewUserLogin(sessionManager)
 	// dex setting ends
 	r := mux.NewRouter().StrictSlash(false)
 	r.PathPrefix("/api/dex").HandlerFunc(dexProxy)
@@ -51,6 +54,35 @@ func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
+	r.HandleFunc("/user/login", func(writer http.ResponseWriter, request *http.Request) {
+		up := &userNamePassword{}
+		decoder := json.NewDecoder(request.Body)
+		err := decoder.Decode(up)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		token, err := loginService.CreateLoginSession(up.Username, up.Password)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(writer, fmt.Errorf("invalid username or password").Error(), http.StatusForbidden)
+			return
+		}
+		response := make(map[string]interface{})
+		response["token"] = token
+		http.SetCookie(writer, &http.Cookie{Name: "argocd.token", Value: token, Path: "/"})
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		b, err := json.Marshal(response)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		writer.Write(b)
+		return
+	}).Methods("POST")
 	r.HandleFunc("/hello", helloHandler)
 	log.Println("Listing for requests at http://localhost:8000/hello")
 	server := &http.Server{
@@ -72,4 +104,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type userNamePassword struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=6"`
 }
