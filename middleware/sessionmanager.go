2192be4d376e82261c506b21104ba3eff81403cb
nishant <nishant@devtron.ai>
2022-01-28 08:00:23 +0530

    session manager config exposed

--- middleware/sessionmanager.go
+++ middleware/sessionmanager.go
@@ -82,6 +82,10 @@ func (mgr *SessionManager) GetUserSessionDuration() time.Duration {
 	return mgr.settings.UserSessionDuration
 }
 
+func (mgr *SessionManager) UpdateSettings(settings *oidc.Settings) {
+	mgr.settings = settings
+}
+
 // Create creates a new token for a given subject (user) and returns it as a string.
 // Passing a value of `0` for secondsBeforeExpiry creates a token that never expires.
 func (mgr *SessionManager) Create(subject string, secondsBeforeExpiry int64, id string) (string, error) {
