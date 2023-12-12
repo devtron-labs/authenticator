2140e6023bacd1a1c99c63722cd76610f688de7d
adi6859 <aditya.ar1909@gmail.com>
2023-12-12 12:00:24 +0530

    commit for testing purpose 4

--- main.go
+++ main.go
@@ -58,7 +58,8 @@ func main() {
 	dexServerAddress = flag.String("dexServerAddress", "http://127.0.0.1:5556", "dex endpoint")
 	dexCLIClientID = flag.String("dexCLIClientID", "argo-cd", "dex clinet id")
 	flag.Parse()
-	client, err := client2.NewK8sClient(new(client2.RuntimeConfig))
+	runtimeConfig, _ := client2.GetRuntimeConfig()
+	client, err := client2.NewK8sClient(runtimeConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
